04184ff4fdae8a7d2bb41d04936ec04c94349916
AnandhiArivazhagan <anandhiarivazhagan30@gmail.com>
2026-02-23 10:21:29 +0530

    test
    Signed-off-by: AnandhiArivazhagan <anandhiarivazhagan30@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const Book = require("./model/book");   // ✅ correct model
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));app.use(cors());
 app.use(cors());
-
 // ===== DATABASE CONNECTION =====
 mongoose.connect("mongodb+srv://Anandhi:anandhi_2005@cluster0.favzmu6.mongodb.net/libraryDB?appName=Cluster0")
   .then(() => console.log("✅ Connected to MongoDB"))
